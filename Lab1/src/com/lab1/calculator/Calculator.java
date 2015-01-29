e313100cba22fbde200ba464e01fbf7868e90912
Kevin Rothenberger <kevin.rothenberger@gmail.com>
2015-01-28 21:36:18 -0500

    Added Calculator class

--- Lab1/src/com/lab1/calculator/Calculator.java
+++ Lab1/src/com/lab1/calculator/Calculator.java
@@ -1,5 +1,27 @@
 package com.lab1.calculator;
 
 public class Calculator {
+	
+	public Calculator(){}
+	
+	public int add(int arg1, int arg2) {
+		return arg1+arg2;
+	}
+	
+	public int subtract(int arg1, int arg2) {
+		return arg1-arg2;
+	}
+	
+	public int multiply(int arg1, int arg2) {
+		return arg1*arg2;
+	}
+	
+	public int divide(int arg1, int arg2) {
+		return arg1/arg2;
+	}
+	
+	public boolean isEqual(int arg1, int arg2) {
+		return arg1 == arg2;
+	}
 
 }
